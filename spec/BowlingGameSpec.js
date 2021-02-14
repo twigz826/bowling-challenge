78f677be8201d277e5ce8f4db86e27417bdeb230
twigz826 <thomastwigden@gmail.com>
2021-02-14 18:34:52 -0300

    Second test passes, refactored

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -3,8 +3,22 @@
 describe('BowlingGame', function(){
 
   let game;
-
-  it('can play a game of bowling', function(){
+  beforeEach(function(){
     game = new BowlingGame();
   })
+
+  it('can roll a gutter game', function(){
+    for (let i = 0; i < 10; i++) {
+      game.roll(0,0)
+    }
+    expect(game.showScore()).toEqual(0)
+  })
+
+  it('can roll a game with all 1s', function(){
+    for (let i = 0; i < 10; i++) {
+      game.roll(1,1)
+    }
+    expect(game.showScore()).toEqual(20)
+  })
+
 })
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,5 +1,21 @@
 'use strict';
 
-class BowlingGame{
+class BowlingGame {
+
+  constructor(){
+    this.rolls = [];
+    this.score = 0;
+  }
+
+  showScore = () => this.score;
+
+  roll = (roll1,roll2) => {
+    this.rolls.push([roll1,roll2]);
+    this.score += (roll1 + roll2);
+  }
+
+  // updateScore = (roll1,roll2) => {
+  //   this.score += (roll1 + roll2);
+  // }
 
 }
